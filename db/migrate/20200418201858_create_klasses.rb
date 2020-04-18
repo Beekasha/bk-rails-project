1c0436c754ce2bf7cb2c62846ca312845acb0668
Beekasha <brian.j.kleiner@gmail.com>
2020-04-18 17:20:22 -0400

    fix migrations

--- db/migrate/20200418201858_create_klasses.rb
+++ db/migrate/20200418201858_create_klasses.rb
@@ -1,6 +1,9 @@
 class CreateKlasses < ActiveRecord::Migration[6.0]
   def change
     create_table :klasses do |t|
+      t.string :name
+      t.integer :period
+      t.integer :teacher_id
 
       t.timestamps
     end
--- db/migrate/20200418202350_create_klasses_students.rb
+++ db/migrate/20200418202350_create_klasses_students.rb
@@ -1,6 +1,8 @@
 class CreateKlassesStudents < ActiveRecord::Migration[6.0]
   def change
     create_table :klasses_students do |t|
+      t.integer :student_id
+      t.integer :klass_id
 
       t.timestamps
     end
